ae6029a96ea92d8d1f85348424b2b67dce3353d8
amora7741 <mora7741@gmail.com>
2025-10-08 13:57:55 -0700

    Add bg to navbar on scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,31 @@
+'use client';
+
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <nav className='fixed inset-x-0 top-4 z-50'>
-      <div className='flex items-center px-8'>
-        <Link href='/'>
-          <span className='text-5xl italic font-medium font-serif tracking-tight md:text-6xl text-white'>
-            sports.
-          </span>
-        </Link>
-      </div>
+    <nav
+      className={`fixed inset-x-4 px-4 py-2 top-4 z-50 flex transition-all ${
+        scrolled ? 'bg-white/10 backdrop-blur-sm rounded-lg' : ''
+      }`}
+    >
+      <Link href='/'>
+        <span className='text-5xl italic font-medium font-serif tracking-tight md:text-6xl text-white'>
+          sports.
+        </span>
+      </Link>
     </nav>
   );
 };
